344e3aac8330283ac710af9b1a19e1258d0ffcfe
ss0710 <63851816+ss0710@users.noreply.github.com>
2020-07-23 00:43:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,42 @@
 var d = new Date();
 document.getElementById("date").innerHTML = "Date: "+d.getDate()+"/" + d.getMonth()+"/" + d.getFullYear();
 
-
-// CODE EXPLAINED channel
-
-// Select the Elements
 const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
 
-// Classes names
+
 const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
 
-// Variables
 let LIST, id;
 
-// get item from localstorage
 let data = localStorage.getItem("TODO");
 
-// check if data is not empty
 if(data){
     LIST = JSON.parse(data);
-    id = LIST.length; // set the id to the last one in the list
-    loadList(LIST); // load the list to the user interface
+    id = LIST.length;
+    loadList(LIST); 
 }else{
-    // if data isn't empty
     LIST = [];
     id = 0;
 }
 
-// load items to the user's interface
+
 function loadList(array){
     array.forEach(function(item){
         addToDo(item.name, item.id, item.done, item.trash);
     });
 }
 
-// clear the local storage
+
 clear.addEventListener("click", function(){
     localStorage.clear();
     location.reload();
 });
 
-// Show todays date
-const options = {weekday : "long", month:"short", day:"numeric"};
-const today = new Date();
-
-dateElement.innerHTML = today.toLocaleDateString("en-US", options);
-
-// add to do function
 
 function addToDo(toDo, id, done, trash){
     
@@ -72,7 +57,6 @@ function addToDo(toDo, id, done, trash){
     list.insertAdjacentHTML(position, item);
 }
 
-// add an item to the list user the enter key
 document.addEventListener("keyup",function(even){
     if(event.keyCode == 13){
         const toDo = input.value;
@@ -98,7 +82,7 @@ document.addEventListener("keyup",function(even){
 });
 
 
-// complete to do
+
 function completeToDo(element){
     element.classList.toggle(CHECK);
     element.classList.toggle(UNCHECK);
@@ -107,17 +91,14 @@ function completeToDo(element){
     LIST[element.id].done = LIST[element.id].done ? false : true;
 }
 
-// remove to do
 function removeToDo(element){
     element.parentNode.parentNode.removeChild(element.parentNode);
     
     LIST[element.id].trash = true;
 }
 
-// target the items created dynamically
-
 list.addEventListener("click", function(event){
-    const element = event.target; // return the clicked element inside list
+    const element = event.target; 
     const elementJob = element.attributes.job.value; // complete or delete
     
     if(elementJob == "complete"){
@@ -126,7 +107,6 @@ list.addEventListener("click", function(event){
         removeToDo(element);
     }
     
-    // add item to localstorage ( this code must be added where the LIST array is updated)
     localStorage.setItem("TODO", JSON.stringify(LIST));
 });
 
